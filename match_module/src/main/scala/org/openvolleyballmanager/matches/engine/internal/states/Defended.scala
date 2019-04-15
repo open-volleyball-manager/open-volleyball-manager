be797f8214e4514c294987f5b33825328f53b515
Michał Perlak <michalperlak@windowslive.com>
2019-04-15 23:37:57 +0200

    Implementing match states

--- match_module/src/main/scala/org/openvolleyballmanager/matches/engine/internal/states/Defended.scala
+++ match_module/src/main/scala/org/openvolleyballmanager/matches/engine/internal/states/Defended.scala
@@ -1,9 +1,12 @@
 package org.openvolleyballmanager.matches.engine.internal.states
 
+import org.openvolleyballmanager.matches.MatchState
 import org.openvolleyballmanager.matches.engine.internal.ActionState
+import org.openvolleyballmanager.player.Player
 
-class Defended extends ActionState {
+class Defended(defender: Player, matchState: MatchState) extends ActionState {
   override def actionFinished(): Boolean = false
 
-  override def execute(): ActionState = ???
+  override def execute(): ActionState = new BadReceive(defender, matchState)
+    .execute()
 }
--- match_module/src/main/scala/org/openvolleyballmanager/matches/engine/internal/states/GoodReceive.scala
+++ match_module/src/main/scala/org/openvolleyballmanager/matches/engine/internal/states/GoodReceive.scala
@@ -1,10 +1,16 @@
 package org.openvolleyballmanager.matches.engine.internal.states
 
 import org.openvolleyballmanager.matches.engine.internal.ActionState
+import org.openvolleyballmanager.matches.{MatchState, PlayerRole}
 import org.openvolleyballmanager.player.Player
 
-class GoodReceive(receiver: Player) extends ActionState {
+class GoodReceive(receiver: Player, matchState: MatchState) extends ActionState {
   override def actionFinished(): Boolean = false
 
-  override def execute(): ActionState = ???
+  override def execute(): ActionState = {
+    val receiverTeam = matchState.team(receiver)
+    val receiverTeamPlacing = matchState.placing(receiverTeam)
+    val setter = receiverTeamPlacing.player(PlayerRole.Setter)
+    new Set(setter, matchState)
+  }
 }
