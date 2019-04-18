ca8e3ca6c8b5308a18df10e937b3f9fa5b77ea4b
Michał Perlak <michalperlak@windowslive.com>
2019-04-18 21:54:56 +0200

    Implementing action states

--- match_module/src/main/scala/org/openvolleyballmanager/matches/engine/internal/states/Set.scala
+++ match_module/src/main/scala/org/openvolleyballmanager/matches/engine/internal/states/Set.scala
@@ -1,9 +1,11 @@
 package org.openvolleyballmanager.matches.engine.internal.states
 
-import org.openvolleyballmanager.matches.MatchState
 import org.openvolleyballmanager.matches.engine.internal.ActionState
+import org.openvolleyballmanager.matches.{MatchState, PlayerRole}
 import org.openvolleyballmanager.player.Player
 import org.openvolleyballmanager.util.random.EventProbability
+
+import scala.util.Random
 
 class Set(setter: Player, matchState: MatchState) extends ActionState {
   override def actionFinished(): Boolean = false
@@ -62,7 +64,17 @@ class Set(setter: Player, matchState: MatchState) extends ActionState {
     technique.coefficient * decisions.coefficient + creativity.coefficient * intuition.coefficient * experience.coefficient
   }
 
-  private def determineAttacker(): Player = ???
+  private def determineAttacker(): Player = {
+    val team = matchState.team(setter)
+    val placing = matchState.placing(team)
+    val attackers = List(
+      placing.player(PlayerRole.Opposite),
+      placing.player(PlayerRole.WingSpiker_1),
+      placing.player(PlayerRole.WingSpiker_2),
+      placing.player(PlayerRole.MiddleBlocker_1)
+    )
+    attackers(new Random().nextInt(attackers.size))
+  }
 
   private def eventWithProbability(probability: Double, nextStateSupplier: () => ActionState): Option[ActionState] =
     if (EventProbability.happens(probability))
