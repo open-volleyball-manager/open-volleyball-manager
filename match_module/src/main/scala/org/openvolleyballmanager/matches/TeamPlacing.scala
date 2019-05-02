7f11ac04eccf8c612d2bc303d51ec5bf4974ff8a
Michal Perlak <michalperlak@windowslive.com>
2019-05-02 13:40:49 +0200

    Cleanup

--- match_module/src/main/scala/org/openvolleyballmanager/matches/TeamPlacing.scala
+++ match_module/src/main/scala/org/openvolleyballmanager/matches/TeamPlacing.scala
@@ -3,7 +3,7 @@ package org.openvolleyballmanager.matches
 import org.openvolleyballmanager.matches.PlayerRole.PlayerRole
 import org.openvolleyballmanager.player.Player
 
-class TeamPlacing(private val players: Map[PlayerRole, Player]) {
+class TeamPlacing(players: Map[PlayerRole, Player]) {
   def server(): Player = ???
 
   def player(role: PlayerRole): Player = players(role)
